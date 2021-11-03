2bceefdbb3b40eeda981bc7b90918949aa868740
B6202385 <89530388+B6202385@users.noreply.github.com>
2021-11-04 00:37:12 +0700

    Create staff.go

---
+++ backend/entity/staff.go
@@ -0,0 +1,13 @@
+package entity
+
+import (
+	"gorm.io/gorm"
+)
+type Staff struct {
+	gorm.Model
+	Name     string
+	Email    string `gorm:"uniqueIndex"`
+	Password string
+	//ProductStocks []ProductStock `gorm:"foreignKey:StaffID"`
+	Returns []Return `gorm:"foreignKey:StaffID"`
+}
