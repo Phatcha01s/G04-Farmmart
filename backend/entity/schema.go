07a3b8e6f589d2fc21e69d0fec5ecb943663ed11
B6202385 <89530388+B6202385@users.noreply.github.com>
2021-11-04 14:50:36 +0700

    Create schema.go

---
+++ backend/entity/schema.go
@@ -0,0 +1,176 @@
+package entity
+
+import (
+	"time"
+	"gorm.io/gorm"
+)
+
+type Contact struct {
+	gorm.Model
+	Ctype    string
+	Accounts []Account `gorm:"foreignKey:ContactID"`
+}
+type Sex struct {
+	gorm.Model
+	Stype    string
+	Accounts []Account `gorm:"foreignKey:SexID"`
+}
+
+type Olduser struct {
+	gorm.Model
+	Otype    string
+	Accounts []Account `gorm:"foreignKey:OlduserID"`
+}
+type Religion struct {
+	gorm.Model
+	Rtype    string
+	Accounts []Account `gorm:"foreignKey:ReligionID"`
+}
+
+type User struct {
+	gorm.Model
+	Name  string
+	Email string  `gorm:"uniqueIndex"`
+	Password string
+	
+	Order []Order `gorm:"foreignkey:OwnerID"`
+	Returns []Return `gorm:"foreignKey:OwnerID"`
+}
+
+type Staff struct {
+	gorm.Model
+	Name     string
+	Email    string `gorm:"uniqueIndex"`
+	Password string
+	ProductStocks []ProductStock `gorm:"foreignKey:StaffID"`
+	Returns []Return `gorm:"foreignKey:StaffID"`
+}
+
+type Supplier struct {
+	gorm.Model
+	Name          string
+	ProductStocks []ProductStock `gorm:"foreignKey:SupplierID"`
+}
+
+type ProductType struct {
+	gorm.Model
+	Ptype    string
+	Products []Product `gorm:"foreignKey:ProductTypeID"`
+}
+
+type Product struct {
+	gorm.Model
+	Name string
+
+	ProductTypeID *uint
+	ProductType   ProductType    `gorm:"references:id"`
+	ProductStocks []ProductStock `gorm:"foreignKey:ProductID"`
+}
+
+
+type PaymentMethod struct {
+	gorm.Model
+	Method  string
+	Payment []Payment `gorm:"foreignKey:PaymentMethodID"`
+}
+
+type DeliveryType struct {
+	gorm.Model
+	Type    string
+	Payment []Payment `gorm:"foreignKey:DeliveryTypeID"`
+}
+//ระบบย่อย ระบบ
+type Account struct {
+	gorm.Model
+	Address    string
+	Province   string
+	OwnerID    *uint
+	Owner      User `gorm:"references:id"`
+	ContactID  *uint
+	Contact    Contact `gorm:"references:id"`
+	SexID      *uint
+	Sex        Sex `gorm:"references:id"`
+	OlduserID  *uint
+	Olduser    Olduser `gorm:"references:id"`
+	ReligionID *uint
+	Religion   Religion `gorm:"references:id"`
+}
+
+//ระบบย่อย ระบบสั่งจองสินค้า
+type Preorder struct {
+	gorm.Model
+	Amount int
+
+	UserID *uint
+	User   User `gorm:"references:id"`
+
+	ProductID *uint
+	Product   Product `gorm:"references:id"`
+
+	PaymentMethodID *uint
+	PaymentMethod   PaymentMethod `gorm:"references:id"`
+}
+
+//ระบบย่อย ระบบสั่งซื้อสินค้า
+type Order struct {
+	gorm.Model
+	PreorderID uint 
+	StatusID uint
+	Ordertime  time.Time
+
+	UserID    *uint
+	User      User		`gorm:"references:id"`
+
+	Payment    []Payment `gorm:"foreignKey:OrderID"`
+	Returns    []Return `gorm:"foreignKey:OrderID"`
+}
+
+//ระบบย่อย ระบบคลังสินค้า
+type ProductStock struct {
+	gorm.Model
+
+	Price  int
+	Amount int
+	
+	ProductID *uint
+	Product   Product `gorm:"references:id"`
+	
+	SupplierID *uint
+	Supplier   Supplier `gorm:"references:id"`
+
+	StaffID *uint
+	Staff   Staff `gorm:"references:id"`
+
+	ProductTime time.Time
+}
+//ระบบย่อย ระบบขอคืนสินค้า
+type Return struct {
+	gorm.Model
+	OwnerID *uint
+	Owner   User `gorm:"references:id"`
+
+	OrderID *uint
+	Order   Order `gorm:"references:id"`
+
+	StaffID *uint
+	Staff   Staff `gorm:"references:id"`
+
+	Reason     string
+	Returndate time.Time
+}
+
+//ระบบย่อย ระบบการชำระเงิน
+type Payment struct {
+	gorm.Model
+	Phone	string
+	PaymentTime time.Time
+
+	OrderID *uint
+	Order   Order	`gorm:"references:id"`
+
+	PaymentMethodID *uint
+	PaymentMethod   PaymentMethod 	`gorm:"references:id"`
+
+	DeliveryTypeID *uint
+	DeliveryType   DeliveryType	`gorm:"references:id"`
+}
