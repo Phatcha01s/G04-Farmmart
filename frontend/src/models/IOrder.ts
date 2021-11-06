d9e715b574d5788ae3c9aff0905fe1721f062ed7
B6202385 <89530388+B6202385@users.noreply.github.com>
2021-11-06 08:34:36 +0700

    Update IOrder.ts

--- frontend/src/models/IOrder.ts
+++ frontend/src/models/IOrder.ts
@@ -1,8 +1,8 @@
 import internal from "stream";
 
 export interface OrdersInterface {
-    ID: string,
-    PreorderID: string,
-    StatusOrder: string,
-    OwnerID: number,
+    ID: number,
+    PreorderID: number,
+    StatusID: number,
+    UserID: number,
   }
